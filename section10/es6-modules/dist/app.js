45dbab6846505440eb4ee8fbb26fc819c916c3d6
Igor Pustovoy <igorpustovoy1978@gmail.com>
2022-08-03 19:27:38 +0200

    section10

---
+++ section10/es6-modules/dist/app.js
@@ -0,0 +1,5 @@
+import { ProjectList } from "./components/project-list.js";
+import { ProjectInput } from "./components/project-input.js";
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
---
+++ section10/es6-modules/dist/decorators/autobind.js
@@ -0,0 +1,11 @@
+//autobind decorator
+export function Autobind(_, _2, descriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor = {
+        configurable: true,
+        get() {
+            return originalMethod.bind(this);
+        },
+    };
+    return adjDescriptor;
+}
